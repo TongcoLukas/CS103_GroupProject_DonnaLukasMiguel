1e7a270587a42a1dc037cc93ed08f155ad3e4834
Lukas Tongco <270036590@yoobeestudent.ac.nz>
2021-10-21 09:42:12 +1300

    Initial commit with headers

---
+++ gitCollab/gitCollab.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Structure definition
+
+
+// Function prototypes
+
+
+int main()
+{
+    
+    // Lukas
+    cout << "\nThis is from Lukas";
+
+}
+
+// Functions
